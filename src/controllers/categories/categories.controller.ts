b8eb48aeddd3f9ce835fd14d0f17819ac22a2ff2
Joseph Morales Silva <josephzx1@hotmail.com>
2023-10-12 22:37:42 -0600

    feat: delete controller and services root directories

--- src/controllers/categories/categories.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Param } from '@nestjs/common';
-
-@Controller('categories')
-export class CategoriesController {
-  @Get('/:id/products/:productId')
-  categories(@Param('id') id: string, @Param('productId') productId: string) {
-    return ` Category : ${id}, Product id: ${productId}`;
-  }
-}
