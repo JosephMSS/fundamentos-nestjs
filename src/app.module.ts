3de853c57e4e264d7309688f9850226d6b0568e6
Joseph Morales Silva <josephzx1@hotmail.com>
2023-10-12 22:40:03 -0600

    feat: update app module, register product, user, and orders modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products/products.controller';
-import { CategoriesController } from './controllers/categories/categories.controller';
-import { ProductsService } from './services/products/products.service';
 import { ProductsModule } from './products/products.module';
-import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductsService],
+  imports: [ProductsModule, UsersModule, OrdersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
