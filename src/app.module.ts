406b3777f70cfe5981f81a2a355c40d4514b31bf
Joseph Morales Silva <josephzx1@hotmail.com>
2023-10-20 08:07:52 -0600

    feat: update app.module, create validation schemas for env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import config from '@src/config';
+import { environments } from 'environments';
 import { firstValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BrandsModule } from './brands/brands.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CustomersModule } from './customers/customers.module';
+import { DatabaseModule } from './database/database.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
-import { environments } from 'environments';
-import config from '@src/config';
+import * as Joi from 'joi';
 @Module({
   imports: [
     ProductsModule,
@@ -23,10 +24,18 @@ import config from '@src/config';
     BrandsModule,
     HttpModule,
     DatabaseModule,
+    /**
+     * forRoot() method is used to configure the module and it's global
+     *  */
     ConfigModule.forRoot({
       envFilePath: environments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
       load: [config],
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AppController],
