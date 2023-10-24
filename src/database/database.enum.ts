2b6a3dec079941171e080c0352b9fed95830198c
Joseph Morales Silva <josephzx1@hotmail.com>
2023-10-24 09:00:23 -0600

    feat: update database module, create pg connection without orm

---
+++ src/database/database.enum.ts
@@ -0,0 +1,4 @@
+export const DATABASE_PROVIDERS = {
+  API_KEY: 'API_KEY',
+  PG: 'PG',
+};
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,9 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { config } from '@src/config';
+import { Client } from 'pg';
+import { DATABASE_PROVIDERS } from './database.enum';
+
 const API_KEY = '12345634';
 /**
  * When a module is global, it can be imported by any other module in the application.
@@ -7,10 +12,27 @@ const API_KEY = '12345634';
 @Module({
   providers: [
     {
-      provide: 'API_KEY',
+      provide: DATABASE_PROVIDERS.API_KEY,
       useValue: API_KEY,
     },
+    {
+      provide: DATABASE_PROVIDERS.PG,
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, db, password, port } =
+          configService.database.postgres;
+        const client = new Client({
+          user,
+          host,
+          database: db,
+          password,
+          port,
+        });
+        client.connect();
+        return client;
+      },
+      inject: [config.KEY],
+    },
   ],
-  exports: ['API_KEY'],
+  exports: [DATABASE_PROVIDERS.API_KEY, DATABASE_PROVIDERS.PG],
 })
 export class DatabaseModule {}
