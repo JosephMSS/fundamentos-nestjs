4dedd37a77b408590c3290899a9214ee418498c7
Joseph Morales Silva <josephzx1@hotmail.com>
2023-10-25 18:14:48 -0600

    feat: update product entity, use typeorm decorators

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,34 @@
+import { Column, PrimaryGeneratedColumn, Entity } from 'typeorm';
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true,
+  })
   name: string;
+  @Column({
+    type: 'text',
+    nullable: false,
+  })
   description: string;
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    nullable: true,
+  })
   price: number;
+  @Column({
+    type: 'int',
+    nullable: false,
+  })
   stock: number;
+  @Column({
+    type: 'varchar',
+  })
   image: string;
 }
