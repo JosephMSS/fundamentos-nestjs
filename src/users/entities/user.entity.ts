e8426ac7d6a70ec79e48e48e64b1a966bb5cacdf
Joseph Morales Silva <josephzx1@hotmail.com>
2023-11-02 21:48:55 -0600

    feat: create user entity with typeorm

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,45 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Roles } from '../enums';
+@Entity()
 export class User {
+  @PrimaryColumn()
   id: number;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true,
+  })
   email: string;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
   password: string;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    enum: Roles,
+  })
   role: string;
+  @CreateDateColumn({
+    type: 'timestamptz',
+    name: 'created_at',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    name: 'updated_at',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
 }
