0268ed9c5ebcb829aee7c24127dde7dd0531945b
Joseph Morales Silva <josephzx1@hotmail.com>
2023-11-02 21:15:32 -0600

    feat: update products service, remove products array

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,28 +1,10 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import {  Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto, UpdateProductDto } from './dto';
 import { Product } from './entities';
 @Injectable()
 export class ProductsService {
-  private products: Product[] = [
-    {
-      id: 0,
-      name: 'Camisa 1',
-      description: 'lorem lorem lorem',
-      price: 1000,
-      stock: 100,
-      image: 'https://i.imgur.com/U4iGx1j.jpeg',
-    },
-    {
-      id: 1,
-      name: 'Camisa 2',
-      description: 'lorem lorem lorem',
-      price: 1000,
-      stock: 100,
-      image: 'https://i.imgur.com/U4iGx1j.jpeg',
-    },
-  ];
   constructor(
     @Inject('API_KEY') private apiKey: string,
     @InjectRepository(Product) private productRepository: Repository<Product>,
