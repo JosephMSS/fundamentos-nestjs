1cf82a96c57732ddf30457703a67cafadd21b0a8
Joseph Morales Silva <josephzx1@hotmail.com>
2023-11-07 13:02:42 -0600

    feat: reate brand entity, and generate migration

--- src/brands/entities/brand.entity.ts
+++ src/brands/entities/brand.entity.ts
@@ -1,5 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Brand {
+  @PrimaryGeneratedColumn()
   id: string;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true,
+  })
   name: string;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
   image: string;
 }
