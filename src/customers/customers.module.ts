26e428b844372019496a7818e73c786cb47f297b
Joseph Morales Silva <josephzx1@hotmail.com>
2023-11-10 18:49:31 -0600

    feat: Import TypeOrmModule and reorder imports in CustomersModule

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CustomersService } from './customers.service';
-import { CustomersController } from './customers.controller';
-import { Customer } from './entities';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
+import { Customer } from './entities';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Customer])],
