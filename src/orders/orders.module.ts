3cf6816f1801130257e77d27625f10cd81f605fb
Joseph Morales Silva <josephzx1@hotmail.com>
2023-12-12 18:21:56 -0600

    Refactor orders module and service

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from '@src/customers/entities';
+import { OrderItem } from './entities/order-item.entity';
+import { Order } from './entities/order.entity';
 import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Customer])],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,19 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
+import { OrderItem } from './entities/order-item.entity';
+import { NotFoundError } from 'rxjs';
+import { Customer } from '@src/customers/entities';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    @InjectRepository(Order) private orderRepository: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private orderItemRepository: Repository<OrderItem>,
+    @InjectRepository(Customer)
+    private customerRepository: Repository<Customer>,
+  ) {}
+  async create(createOrderDto: CreateOrderDto) {
+    const order = new Order();
+    if (createOrderDto.customerId) {
+      const customer = await this.customerRepository.findOneBy({
+        id: createOrderDto.customerId,
+      });
+      if (!customer) {
+        throw new NotFoundException('Customer not found');
+      }
+      order.customer = customer;
+    }
+    const newOrder = await this.orderRepository.save(order);
+    return newOrder;
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll() {
+    const orders = await this.orderRepository.find();
+    return orders;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} order`;
+    const order = this.orderRepository.findOneBy({
+      id,
+    });
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+    return order;
   }
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
