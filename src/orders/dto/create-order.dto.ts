027d8db15b2e0ab04ac484de3c5eb0ded411d370
Joseph Morales Silva <josephzx1@hotmail.com>
2023-12-12 18:21:48 -0600

    Add validation for customerId in CreateOrderDto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,9 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPositive } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsPositive()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly customerId: number;
+}
